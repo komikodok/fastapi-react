d5b9bd99c3e9aaf7834077dc4a227750c8683bb4
komikodok <komikodok666@gmail.com>
2025-03-16 22:12:43 +0700

    BIG UPDATED

--- serializer/base.py
+++ serializer/base.py
@@ -5,15 +5,15 @@ from itertools import starmap
 
 class BaseSerializer:
 
-    def __init__(self, doc_ref: List[DocumentSnapshot] | DocumentSnapshot):
-        self.doc_ref = doc_ref
+    def __init__(self, doc_snaps: List[DocumentSnapshot] | DocumentSnapshot):
+        self.doc_snaps = doc_snaps
 
     @property
     def data(self):
         fields = self.Meta.fields
         
-        if isinstance(self.doc_ref, list):
-            docs = self.doc_ref
+        if isinstance(self.doc_snaps, list):
+            docs = self.doc_snaps
             to_dict = lambda doc: {"id": doc.id, **doc.to_dict()}
             datas = list(map(to_dict, docs))
 
@@ -25,7 +25,7 @@ class BaseSerializer:
             filter_datas = list(map(filter_list_data, datas))
             return filter_datas
 
-        doc = self.doc_ref
+        doc = self.doc_snaps
         data = {"id": doc.id, **doc.to_dict()}
 
         filter_field = lambda field, value: (field, value) if field in fields else None
